82ad2e51b76ea7d399b22654d7b215f97f32d6f4
Kopi <ihessmc@gmail.com>
2015-11-23 13:55:36 -0800

    Update Command_explode.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_explode.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_explode.java
@@ -1,1 +1,48 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
+
+import me.StevenLawson.TotalFreedomMod.UFM_Util;
+import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
+
+@CommandPermissions(level = AdminLevel.ALL, source = SourceType.BOTH)
+@CommandParameters(description = "Explode a player", usage = "/<command> <playername>")
+public class Command_explode extends TFM_Command {
+  
+  @Override
+  public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole) {
+    
+        if (!sender.getName().equals("iHess")) {
+            sender_p.sendMessage(ChatColor.RED + "You shall now explode!\nDon't access this command, please!");
+            sender_p.setGameMode(GameMode.SURVIVAL);
+            sender_p.setHealth(0);
+            smite(sender_p);
+            sender_p.getWorld().createExplosion(player.getLocation().getX(), player.getLocation().getY(), player.getLocation().getZ(), 10f, false, false);
+        }
+        
+        if (args.length != 1)
+        {
+            return false;
+        }
+
+        final Player player = getPlayer(args[0]);
+
+        if (player == null)
+        {
+            sender.sendMessage(TFM_Command.PLAYER_NOT_FOUND);
+            return true;
+        }
+        
+        TFM_Util.adminAction(sender_p.getName(), "Exploding " + player.getName() + " to their very death!", true);
+        player.setGameMode(GameMode.SURVIVAL);
+        player.setHealth(0);
+        smite(player);
+        player.getWorld().createExplosion(player.getLocation().getX(), player.getLocation().getY(), player.getLocation().getZ(), 10f, false, false);
+      
+  }
+}
