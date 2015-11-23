a70b8784aa326495a02e1fd20f1217830df0ad66
Kopi <ihessmc@gmail.com>
2015-11-23 13:43:41 -0800

    Create Command_swag.java

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_swag.java
@@ -0,0 +1,6 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+@CommandPermissions(level = AdminLevel.ALL, source = SourceType.BOTH)
+@CommandParameters(description = "LOLOLOL", usage = "/<command> <playername>")
+public class Command_optroll extends TFM_Command {
+}
