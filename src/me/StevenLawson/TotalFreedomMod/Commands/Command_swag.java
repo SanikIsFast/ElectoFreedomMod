d295b1f38e453c12bb67952026d64b07d9f7d68b
Kopi <ihessmc@gmail.com>
2015-11-23 13:44:08 -0800

    Delete Command_swag.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_swag.java
+++
@@ -1,6 +0,0 @@
-package me.StevenLawson.TotalFreedomMod.Commands;
-
-@CommandPermissions(level = AdminLevel.ALL, source = SourceType.BOTH)
-@CommandParameters(description = "LOLOLOL", usage = "/<command> <playername>")
-public class Command_optroll extends TFM_Command {
-}
