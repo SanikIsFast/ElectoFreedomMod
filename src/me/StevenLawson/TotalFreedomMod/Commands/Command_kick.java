4e55e15dd188adfeae62987ff82d6515e7ad1694
Kopi <ihessmc@gmail.com>
2015-11-23 10:08:46 -0800

    Create Command_kick.java

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_kick.java
@@ -0,0 +1,41 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
+@CommandParameters(description = "Kicks player", usage = "/<command> <playername>")
+public class Command_kick extends TFM_Command
+{
+    @Override
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        if (args.length == 0)
+        {
+            return false;
+        }
+
+        final Player player = getPlayer(args[0]);
+
+        if (player == null)
+        {
+            playerMsg(TFM_Command.PLAYER_NOT_FOUND,ChatColor.RED);
+            return true;
+        }
+
+        String reason = null;
+        if (args.length >= 2)
+        {
+            reason = StringUtils.join(ArrayUtils.subarray(args, 1, args.length), " ");
+        }
+        TFM_Util.adminAction(sender.getName(), "Kicked " + player.getName() + (reason != null ? (" for : " + ChatColor.YELLOW + reason) : ""), false);
+        player.kickPlayer(ChatColor.RED + "You are kicked by " + ChatColor.YELLOW + sender.getName() + (reason != null ? ("\nReason: " + ChatColor.YELLOW + reason) : ""));
+
+        return true;
+    }
+}
